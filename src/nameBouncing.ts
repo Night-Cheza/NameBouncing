7abc7022fa0990ae9f4712087b00d5580ea7b594
Night-Cheza <leila.kassi@outlook.com>
2021-03-30 19:17:55 -0600

    continue to figure out

--- src/nameBouncing.ts
+++ src/nameBouncing.ts
@@ -1,126 +1,43 @@
-/*interface CanvasPrinting {
-    viewport: HTMLCanvasElement;
-    ctx: CanvasRenderingContext2D;
-}
-
-
-
-class NameBounce {
-    //nameInput: HTMLInputElement
-    canvas: HTMLCanvasElement;
-    x: number;
-    y: number;
-    btnEl: HTMLButtonElement;
-    //viewport: HTMLCanvasElement;
-    
-        
-    constructor () {
-        
-        this.btnEl = <HTMLButtonElement> document.querySelector("button");
-        btn = this.btnEl.addEventListener("submit")
-        //const name = nameInput.value;
-        this.canvas = <HTMLCanvasElement> document.querySelector("#.bounce-field");
-        this.x = this.canvas.height;
-        this.y = this.canvas.width;
-        const btn = document.querySelector("button");
-        //btn!.addEventListener("submit", (e: Event) =>this.name);
-        const ctx = this.canvas.getContext('2d')!;
-
-        
-    } 
-
-   // nameInput = <HTMLFormElement> document.querySelector("form")!;
-       // nameInput.addEventListener("submit", event => {
-          //  const name = <HTMLInputElement> document.getElementById("text").value
-            
-          //  const newName = new NameBounce ()
-
-           // console.log(newName);
-            
-        })
-
-    function takeAndMove (btn) {
-    
-    function () {
-
-    }
-    
-}
-
-
-const Bounce = new NameBounce ()
-
-function () {
-console.log(Bounce.name)
-}
-
-
-           /* ctx.beginPath();
-            ctx.rect(20, 40, 50, 50);
-            ctx.fillStyle = "#FF0000";
-            ctx.fill();
-            ctx.closePath();
-            const nameRadius = 25;
-
-}
-
-
-
-let n = new NameBounce ();
-
-
-courseForm.addEventListener("submit", event =>{
-    event.preventDefault(); //if I fill the form, there will not be http request - for now
-    const titleEl = document.getElementById("title") as HTMLInputElement;
-    const priceEl = document.getElementById("price") as HTMLInputElement;
-
-    const title = titleEl.value;
-    const price = +priceEl.value;
-
-    const createdCourse = new Course(title, price);
-
-    if(!validate(createdCourse)) {
-        alert ("Invalid imput. Please try again");
-        return;
-    }*/
-
-interface CanvasDrawPath {  //look in typescript lab
-    beginPath (): void;
-    isPointInPath(path: Path2D, x: number, y: number, fillRule?: CanvasFillRule): boolean
-
-}
-
 class NameBounce {
     container: HTMLDivElement;
+    form: HTMLFormElement;
     nameField: HTMLFormElement;
     enteredName: HTMLInputElement;
     canvas: HTMLCanvasElement;
+    button: HTMLButtonElement;
+    nameToCanvas: CanvasRenderingContext2D;
+    nameInput: string;
 
     constructor () {
         this.container = <HTMLDivElement> document.getElementById("container");
-        this.nameField = <HTMLFormElement> this.container.firstElementChild;
-        this.nameField.id = "input";
-        this.enteredName = <HTMLInputElement> this.nameField.getElementById("name")!;
-        this.canvas = <HTMLCanvasElement> document.querySelector(".bounce-field");
-
-        const bouncingName = this.canvas.getContext("2d");
-        bouncingName!.fillText(this.enteredName.value, 50, 50);
-
-        const x = this.canvas.width/2;
-        const y = this.canvas.height-30;
-       
-        //this.drawText();  
-        this.buttonHandler();  
+        this.form = <HTMLFormElement> document.querySelector("form")
+        this.nameField = <HTMLFormElement> this.form.firstElementChild!;
+        this.nameField.id = "name-field";        
+        this.enteredName = <HTMLInputElement>this.nameField.querySelector("input");
+        this.nameInput = this.enteredName.value;
         
+        this.button = <HTMLButtonElement> document.querySelector("button");
+        this.canvas = <HTMLCanvasElement> document.getElementById("bounce-field");
+        this.nameToCanvas = <CanvasRenderingContext2D> this.canvas.getContext("2d")!;
+        this.nameToCanvas.font = "35px Sans-serif";
+      
+        this.buttonHandler();  
     }
 
-    private Input(): string {
-        const resultName = this.enteredName.value
+    private submitName (event: Event) {
+        event.preventDefault();
+        const userInput = this.enteredName.value;
+        console.log (userInput)
+        this.clearInputs();
+    }
 
-        if (!resultName) {
+    private buttonHandler () {
+        this.button.addEventListener ("submit", this.submitName);
+
+        if (!this.enteredName.value) {
             throw new Error ("Invalid input. Please enter your name");
         } else {
-            return (resultName);
+            return (this.enteredName.value);
         }
     }
 
@@ -128,42 +45,26 @@ class NameBounce {
         this.enteredName.value = "";
     }
 
-    private submitName (event: Event) {
-        event.preventDefault();
-        const userInput = this.Input();
-        console.log (this.Input())
-        this.clearInputs();
+    
+
+    private bounceText() {          
+        let bouncingName = this.nameToCanvas.measureText(this.nameInput);
+            
+        const x = this.canvas.width;
+        const y = this.canvas.height;
+
+        const bouncNameWidth = bouncingName.width;
+        const collideNameWidth = ;  
+        this.nameToCanvas.beginPath();
+        this.nameToCanvas()
+        //ctx.fill();
+        //ctx.closePath();
+    
     }
-
-    private buttonHandler () {
-        this.nameField.addEventListener ("submit", this.submitName);
-    }
-
-  //  private bouncingHandler() {
-        
-  //  }
-
-
-   // private draw() {
-    //    this.enteredName.value.beginPath()
-   // }
-
-   
-
-
 }
 
 
-const NameInput = new NameBounce();
-
-/*let Name = <HTMLInputElement> document.getElementById("name-field")!
-Name.addEventListener("submit", () =>
-const nameEl = <HTMLInputElement> document.getElementById("name")
-const enteredName = nameEl.value
 
 const NewName = new NameBounce()
-)
 
-function drawText({ text = "", location: [x, y] = [0, 0], bold = false }) {
-    addEventListener// Draw text
-} */
+console.log(NewName)
