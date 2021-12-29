7679e20770032861ea08d33d5c55e5b440a58056
Leila <leila.kassi@outlook.com>
2021-12-28 18:36:00 -0700

    Create README.md

---
+++ src/nameBouncing.js
@@ -0,0 +1,23 @@
+function getName() {
+    // @ts-ignore
+    var name = document.getElementById("input").value;
+    document.getElementById("bounceName").innerHTML = name;
+    document.getElementById("bounceName").animate([
+        { transform: 'translate(0, 0)' },
+        { transform: 'translate(640px, 440px)' },
+        { transform: 'rotate(180deg)' },
+        { transform: 'translate(0, 440px)' },
+        { transform: 'rotate(-180deg)' },
+        { transform: 'translate(640px, 0)' },
+        { transform: 'rotate(-180deg)' },
+        { transform: 'translate(640px, 440px)' },
+        { transform: 'rotate(180deg)' },
+        { transform: 'translate(640px, 0)' },
+        { transform: 'rotate(180deg)' },
+        { transform: 'translate(0, 440px)' },
+        { transform: 'rotate(180deg)' }
+    ], {
+        duration: 10500,
+        iterations: Infinity
+    });
+}
--- src/nameBouncing.ts
+++ src/nameBouncing.ts
@@ -1,96 +1,28 @@
-class NameBounce {
-    container: HTMLDivElement;
-    form: HTMLFormElement;
-    nameField: HTMLFormElement;
-    enteredName: HTMLInputElement;
-    button: HTMLButtonElement;
-    nameInput: string;
-    textArea: HTMLTextAreaElement;
-     
-        constructor () {
-        this.container = <HTMLDivElement> document.getElementById("container");
-        this.form = <HTMLFormElement> document.querySelector("form")
-        this.nameField = <HTMLFormElement> this.form.firstElementChild!;
-        this.enteredName = <HTMLInputElement>this.nameField.querySelector("input");
-        this.nameInput = "";
-        this.button = <HTMLButtonElement> document.querySelector("button");
-        this.textArea = <HTMLTextAreaElement> document.getElementById("bounce-field");
- 
-        this.buttonHandler(); 
-       // this.clearInputs();
-    }
+function getName() {
+    const name: string = (<HTMLInputElement>document.getElementById("input")).value;
+
+    document.getElementById("bounceName")!.innerHTML = name;
+
+    document.getElementById("bounceName")!.animate ([
+        {transform: 'translate(0, 0)'},
+        {transform: 'translate(640px, 440px)'},
+        {transform: 'rotate(180deg)'},
+        {transform: 'translate(0, 440px)'},
+        {transform: 'rotate(-180deg)'},
+        {transform: 'translate(640px, 0)'},
+        {transform: 'rotate(-180deg)'},
+        {transform: 'translate(640px, 440px)'},
+        {transform: 'rotate(180deg)'},
+        {transform: 'translate(640px, 0)'},
+        {transform: 'rotate(180deg)'},
+        {transform: 'translate(0, 440px)'},
+        {transform: 'rotate(180deg)'}
+
+    ], {
+        duration:10500,
+        iterations: Infinity
+    });
     
-
-    private buttonHandler () {
-        this.form.addEventListener ("submit", (event) =>{ 
-            this.nameInput = this.enteredName.value;
-            event.preventDefault();
-
-            if (!this.nameInput) {
-                throw Error("Invalid input. Please enter your name");
-            } else {
-                console.log(this.nameInput);
-            }            
-        });
-
-        
-    }
-
-   // private clearInputs () {
-        //this.nameInput = "";
-    //}
-}
-   
-        
-class AnimateText {  
-    nameInput: string;
-    textArea: HTMLTextAreaElement;
-
-    constructor () {
-        this.textArea = <HTMLTextAreaElement> document.getElementById("bounce-field");
-        this.nameInput = "";
-        this.textArea.innerHTML = this.nameInput
-        
-        const bouncingName = this.textArea.value;
-    }
-    
-       
-      
-
-
-
-
-
-    //     <script type="text/javascript">
-    //     function RemoveContent () {
-    //         var srcObj = document.getElementById ("src");
-
-    //         if (document.createRange) {     // all browsers, except IE before version 9
-    //             var rangeObj = document.createRange ();
-    //             rangeObj.selectNodeContents (srcObj);
-    //             rangeObj.deleteContents ();
-    //         }
-    //         else {      // Internet Explorer before version 9
-    //             var rangeObj = document.body.createTextRange ();
-    //             rangeObj.moveToElementText (srcObj);
-    //             rangeObj.select ();
-    //             rangeObj.execCommand ('cut');
-    //         }
-    //     }
-    // </script>
-    
-    
-        
-        
-    
-    
-    
-
-
+  
 }
 
-
-
-const NewName = new NameBounce();
-
-//console.log(NewName.enteredName.value);
